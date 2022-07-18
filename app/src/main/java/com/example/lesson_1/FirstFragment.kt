57131120b3cdc761fa0d52e5d04fc46c2ab97149
Vanilla <vetal-vetal-vv@mail.ru>
2022-07-18 19:55:31 +0300

    listView

--- app/src/main/java/com/example/lesson_1/FirstFragment.kt
+++ app/src/main/java/com/example/lesson_1/FirstFragment.kt
@@ -1,11 +1,14 @@
 package com.example.lesson_1
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.example.lesson_1.databinding.FragmentFirstBinding
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -31,17 +34,37 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            sum +=1
+            sum += 1
             binding.textviewFirst.text = sum.toString()
         }
-        binding.buttonFirst1.setOnClickListener{
-            sum +=2
+        binding.buttonFirst1.setOnClickListener {
+            sum += 2
             binding.textviewFirst.text = sum.toString()
         }
-        binding.buttonFirst2.setOnClickListener{
-            sum +=3
+        binding.buttonFirst2.setOnClickListener {
+            sum += 3
             binding.textviewFirst.text = sum.toString()
         }
+
+// определяем строковый массив
+
+// определяем строковый массив
+        val catNames = arrayOf(
+            "Рыжик", "Барсик", "Мурзик", "Мурка", "Васька",
+            "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
+            "Китти", "Масяня" , "Барсик", "Мурзик", "Мурка", "Васька",
+            "Томасина", "Кристина", "Пушок", "Дымка", "Кузя",
+            "Китти", "Масяня"
+        )
+
+// используем адаптер данных
+
+// используем адаптер данных
+        val adapter: ArrayAdapter<String> = ArrayAdapter(
+            requireContext(),
+            R.layout.simple_list_item_1, catNames
+        )
+        binding.listView.setAdapter(adapter)
     }
 
     override fun onDestroyView() {
