7391c4c51a048cc62a1fc2cf93cc547f8b3db597
Vanilla <vetal-vetal-vv@mail.ru>
2022-07-24 19:10:48 +0300

    with let apply

--- app/src/test/java/com/example/lesson_1/test.kt
+++ app/src/test/java/com/example/lesson_1/test.kt
@@ -1,25 +1,44 @@
 package com.example.lesson_1
 
-fun main(){
-    var a = 0
-    var a2 = 1
-    val vika :Vika = Vika()
-    val vika2 = Vika()
-    vika.age
-
-    println(a+a2)
-    println( Vika.numberr)
-
+fun main() {
+    // Функция with позволяет выполнить несколько операций над одним объектом, не повторяя его имени.
+    val vika = Vika()
+//1
+    with(vika) {
+        name()
+    }
+    //2
+    val with = with(vika) {
+        name()
+        1
+    }
+//3
+    val mem = vika.apply {
+        name()
+    }
+    //4
+    vika.apply {
+        name()
+    }
+    //let полезен при работе с объектами, которые могут принимать значение null. Вместо того, чтобы создавать длинные цепочки выражений if-else, можно просто скомбинировать оператор ?
+// («оператор безопасного вызова») с let: в результате мы получим лямбду, у которой аргумент it является не nullable-версией исходного объекта.
+    //5
+    vika?.let { it.name() } ?: println(null)
 }
-class Vika(){
-    val age = 23
 
+
+class Vika() {
+    val age = 23
+    fun name() {
+        println(name + age.toString())
+    }
 
     companion object {
-        const val name:String ="Victory"
+        const val name: String = "Victory"
         var numberr = 0
     }
 }
-fun  chislo():Int{
+
+fun chislo(): Int {
     return 1
 }
